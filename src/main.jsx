9b0ccefb48e00310d955db058bdf79b03400f2f3
hihassan1998 <hassanihussain1998@gmail.com>
2024-04-17 21:11:16 +0200

    integrated CI/CD pilpeline

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
