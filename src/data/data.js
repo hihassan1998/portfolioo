023297da2fab983ec918ea11294730679fedda7c
hihassan1998 <hassanihussain1998@gmail.com>
2024-04-28 20:50:30 +0200

    added new projects

--- src/data/data.js
+++ src/data/data.js
@@ -27,9 +27,9 @@ export const data = [
   {
     id: 4,
     img: 'src/assets/techweb.png',
-    title: "Summize",
+    title: "AI Summarization App",
     desc: "An OpenAi GPT 4 api using website",
-    link: "https://hihassan1998.github.io/portfoliooo/",
+    link: "https://ai-summarizer-5v1r.onrender.com/",
     git: "",
   },
   {
@@ -39,6 +39,14 @@ export const data = [
     desc: "",
     link: "https://hihassan1998.github.io/portfoliooo/",
     git: "",
+  },
+  {
+    id: 6,
+    img: 'src/assets/contact_us.png',
+    title: "Tecknad Portfölj Websida",
+    desc: "",
+    link: "https://github.com/hihassan1998/xrwvm-fullstack_developer_capstone",
+    git: "",
   }
-
+  
 ];
